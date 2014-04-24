7d3efa65e98faae4ecdb7c6dd55a99298874e28e
Sandip Chitale <Sandip.Chitale@emc.com>
2014-04-24 09:46:09 -0700

    Command output

--- js/POM360.js
+++ js/POM360.js
@@ -1,24 +1,27 @@
 (function() {
-    angular.module("POM360App", ['ui.router'])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('POM360', {
-                url: '/',
-                templateUrl : 'templates/effective-pom.html'
-            })
-            .state('effective-pom', {
-                url: '/effective-pom',
-                templateUrl : 'templates/effective-pom.html'
-            })
-            .state('dependencies', {
-                url: '/dependencies',
-                templateUrl : 'templates/dependencies.html'
-            });
-    })
-    .run(function($rootScope, $window, $location) {
+    angular.module("POM360App", ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('POM360', {
+            url : '/',
+            templateUrl : 'templates/effective-pom.html'
+        }).state('effective-pom', {
+            url : '/effective-pom',
+            templateUrl : 'templates/effective-pom.html'
+        }).state('dependencies', {
+            url : '/dependencies',
+            templateUrl : 'templates/dependencies.html'
+        });
+    }).run(function($rootScope, $window, $location) {
 
-    })
-    .controller('POM360Controller', function($scope) {
-        $scope.message = new Date();
+    }).controller('POM360Controller', function($scope) {
+        $scope.runMvn = function() {
+            var effectivePomCommand = $('#effective-pom-command');
+            if (effectivePomCommand) {
+                effectivePomCommand.val($('#mvn').val() + ' help:effective-pom')
+            }
+            var effectivePom = $('#effective-pom');
+            if (effectivePom) {
+                effectivePom.val(effectivePom.val() + '\n' + $('#mvn').val() + ' help:effective-pom')
+            }
+        }
     });
 })();
