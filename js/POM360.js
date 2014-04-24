77cc1b68a8d269882a6a0728621bf87fb961ea16
Sandip Chitale <Sandip.Chitale@emc.com>
2014-04-24 15:34:18 -0700

    Refactoring

--- js/POM360.js
+++ js/POM360.js
@@ -1,4 +1,8 @@
 (function() {
+    var pathExtra = require('path-extra');
+    var gui = require('nw.gui');
+    var openThis = require('open');
+
     angular.module("POM360App", ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
         $stateProvider.state('POM360', {
             url : '/',
@@ -11,8 +15,60 @@
             templateUrl : 'templates/dependencies.html'
         });
     }).run(function($rootScope, $window, $location) {
-
+        $rootScope.openThis = function(fileOrUrl) {
+            openThis(fileOrUrl);
+            return false;
+        }
+        $rootScope.openThisRelativeToHomeDir = function(relativePathToFile) {
+            return $rootScope.openThis(pathExtra.homedir() + '/' + relativePathToFile);
+        }
     }).controller('POM360Controller', function($scope) {
+        $scope.config = {
+            mvnCommand: 'mvn',
+            mvnOptions: '',
+            pomFile: 'pom.xml',
+            settingsFile: '~/.m2/settings.xml'
+        }
+
+        $scope.debug = function() {
+            gui.Window.get().showDevTools();
+        }
+
+        $scope.exit = function() {
+            gui.Window.get().close();
+            return false;
+        }
+
+        // Select mvn command
+        var mvnCommandSelector = $('#mvnCommandSelector');
+        mvnCommandSelector.change(function(evt) {
+            $scope.config.mvnCommand = mvnCommandSelector.val();
+            $scope.$apply();
+        });
+        $scope.selectMvnCommand = function() {
+            mvnCommandSelector.trigger('click');
+        }
+
+        // Select POM file
+        var pomFileSelector = $('#pomFileSelector');
+        pomFileSelector.change(function(evt) {
+            $scope.config.pomFile = pomFileSelector.val();
+            $scope.$apply();
+        });
+        $scope.selectPomFile = function() {
+            pomFileSelector.trigger('click');
+        }
+
+        var settingsFileSelector = $('#settingsFileSelector');
+        settingsFileSelector.change(function(evt) {
+            $scope.config.settingsFile = settingsFileSelector.val();
+            $scope.$apply();
+        });
+
+        $scope.selectSettingsFile = function() {
+            settingsFileSelector.trigger('click');
+        }
+
         $scope.runMvn = function() {
             var effectivePomCommand = $('#effective-pom-command');
             if (effectivePomCommand) {
